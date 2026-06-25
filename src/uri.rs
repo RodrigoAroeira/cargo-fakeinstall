d52efbb91fccc9df55942a25ab8422007ffdf505
RodrigoAroeira <raroeirabr@gmail.com>
2026-06-24 23:40:19 -0300

    fix: expand tilde in Remote Uri

--- src/uri.rs
+++ src/uri.rs
@@ -1,5 +1,7 @@
 use std::str::FromStr;
 
+use expanduser::expanduser;
+
 #[derive(Clone)]
 pub enum Uri {
     Remote(String),
@@ -10,7 +12,8 @@ impl FromStr for Uri {
     type Err = String;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         if let Some(path) = s.strip_prefix("file://") {
-            let absolute = std::path::absolute(path).map_err(|e| e.to_string())?;
+            let expanded = expanduser(path).map_err(|e| e.to_string())?;
+            let absolute = std::path::absolute(expanded).map_err(|e| e.to_string())?;
             Ok(Uri::Local(absolute.display().to_string()))
         } else {
             Ok(Uri::Remote(s.to_string()))
